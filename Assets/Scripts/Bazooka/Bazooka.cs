b8b0db3e6e7338f242a5c242bc6affc55a64bc56
DavidTODev <davidtobon.dat@gmail.com>
2022-05-24 21:00:06 -0500

    David T added basic bazooka and ammo

--- Assets/Scripts/Bazooka/Bazooka.cs
+++ Assets/Scripts/Bazooka/Bazooka.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Bazooka : MonoBehaviour
 {
+
+    [SerializeField] private int maxAmmo;
+
+    int currentAmmoQuantity;
+
+    [SerializeField] private float shootDelay;
+    [SerializeField] private float impulseDelay;
+    [SerializeField] private Ammo ammoType;
+
+    bool canShoot;
+
+    bool canImpulse;
+
+    [SerializeField] Transform barrelExitPoint;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canShoot = true;
+        canImpulse = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reload()
     {
-        
+        currentAmmoQuantity = maxAmmo;
     }
+
+    public void Shoot()
+    {
+
+        currentAmmoQuantity -= 1;
+
+        if (!canShoot) return;
+
+        StartCoroutine(ShootDelay(shootDelay));
+
+        var bullet = Instantiate(ammoType,this.transform);
+        bullet.transform.position = barrelExitPoint.position;
+        bullet.transform.rotation = barrelExitPoint.rotation;
+        bullet.transform.parent = null;
+    }
+
+    public void Impulse()
+    {
+        if (!canImpulse) return;
+
+        StartCoroutine(ShootDelay(impulseDelay));
+    }
+
+    IEnumerator ShootDelay(float time)
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(time);
+        canShoot = true;
+    }
+
+    IEnumerator ImpulseDelay(float time)
+    {
+        canImpulse = false;
+        yield return new WaitForSeconds(time);
+        canImpulse = true;
+    }
+
+    void ShootFX()
+    {
+
+    }
+
+    void ImpulseFX()
+    {
+
+    }
+
 }
