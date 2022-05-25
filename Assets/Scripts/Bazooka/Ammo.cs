75af3658ec3289b85728372164361aa8eaf9188e
DavidTODev <davidtobon.dat@gmail.com>
2022-05-24 21:19:53 -0500

    David T implemented bullet destruction at range exit

---
+++ Assets/Scripts/Bazooka/Ammo.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ammo : MonoBehaviour
+{
+    float damage;
+
+    [SerializeField] private float damageRange;
+
+    float blastRadious;
+
+    [SerializeField] private float ammoSpeed;
+    Vector3 initialPos;
+
+    private void Update()
+    {
+        Move();
+        CheckEndRange();
+    }
+
+    public void Move()
+    {
+        transform.Translate(this.transform.right * ammoSpeed * Time.deltaTime);   
+    }
+
+    public abstract void ExplosionFX();
+
+    public void CheckEndRange()
+    {
+        if(Vector3.Distance(initialPos,transform.position) > damageRange) Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Bazooka/Missile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : Ammo
+{
+
+    public override void ExplosionFX()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
